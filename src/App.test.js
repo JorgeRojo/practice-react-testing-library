d4d8c27d909073941de40f153cdcb2d4ef8e0482
Jorge Rojo <jrojo@redpoints.com>
2019-07-17 17:16:16 +0200

    react app creation

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+describe('App component', () => {
+  const setup = () => {
+    return render(<App/>);
+  };
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+
+    const { container } = setup();
+    expect(container).to 
+  });
 });
